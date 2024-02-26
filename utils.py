6068fb6b6469698ac5b50bafa36cfef7656f801a
wisdomokubo <39890085+wisdomokubo@users.noreply.github.com>
2024-02-26 02:25:55 +0100

    Add files via upload

--- utils.py
+++ utils.py
@@ -30,6 +30,6 @@ def get_conversation_string():
     conversation_string = ""
     for i in range(len(st.session_state['responses'])-1):
         
-        conversation_string += "You: "+st.session_state['requests'][i] + "\n"
+        conversation_string += "Human: "+st.session_state['requests'][i] + "\n"
         conversation_string += "Bot: "+ st.session_state['responses'][i+1] + "\n"
     return conversation_string
