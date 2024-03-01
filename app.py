3233e83053f03bd183efd7f1bc99de2a99f456ce
wisdomokubo <39890085+wisdomokubo@users.noreply.github.com>
2024-03-01 17:04:00 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from langchain.chat_models import ChatOpenAI
 from langchain.chains import ConversationChain
 from langchain.chains.conversation.memory import ConversationBufferWindowMemory
+import os
 import openai
 from PIL import Image
 from langchain.prompts import (
@@ -12,7 +13,7 @@ from langchain.prompts import (
 import streamlit as st
 from streamlit_chat import message
 from streamlit_option_menu import option_menu
-from utils import *
+
 
 
 st.set_page_config(
@@ -57,8 +58,61 @@ if selected == "About":
     
 elif selected == "Chatbot":
     
- st.write(" In Development. ")
-  
+    if 'responses' not in st.session_state:
+        st.session_state['responses'] = ["Hi, I am Wisdom, a bot designed to teach you indigenous languages. How are you?"]
+        
+    if 'requests' not in st.session_state:
+        st.session_state['requests'] = []
+
+    llm = ChatOpenAI(model_name="gpt-3.5-turbo", openai_api_key=st.secrets["OPENAI_API_KEY"])
+
+    if 'buffer_memory' not in st.session_state:
+                st.session_state.buffer_memory=ConversationBufferWindowMemory(k=3,return_messages=True)
+
+
+    system_msg_template = SystemMessagePromptTemplate.from_template(template="""
+    You are a helpful assistant. 
+    
+    """)
+
+
+    human_msg_template = HumanMessagePromptTemplate.from_template(template="{input}")
+
+    prompt_template = ChatPromptTemplate.from_messages([system_msg_template, MessagesPlaceholder(variable_name="history"), human_msg_template])
+
+    conversation = ConversationChain(memory=st.session_state.buffer_memory, prompt=prompt_template, llm=llm, verbose=True)
+
+
+
+
+# container for chat history
+    response_container = st.container()
+# container for text box
+    textcontainer = st.container()
+
+
+    with textcontainer:
+        query = st.text_input("Query: ", key="input")
+        if query:
+            with st.spinner("typing..."):
+                conversation_string = get_conversation_string()
+                st.code(conversation_string)
+                refined_query = query_refiner(conversation_string, query)
+                #st.subheader("Refined Query:")
+                #st.write(refined_query)
+                context = find_match(refined_query)
+                #print(context)  
+                response = conversation.predict(input=f"Context:\n {context} \n\n Query:\n{query}")
+            st.session_state.requests.append(query)
+            st.session_state.responses.append(response) 
+    with response_container:
+        if st.session_state['responses']:
+
+            for i in range(len(st.session_state['responses'])):
+                message(st.session_state['responses'][i],key=str(i))
+                if i < len(st.session_state['requests']):
+                    message(st.session_state["requests"][i], is_user=True,key=str(i)+ '_user')
+
 elif selected == "Community":
     st.write("""
     Our community is a platform for our users to interact with other users on their language learning journey. It is also a place to get updates on new features, exclusive offers and other essential information. Users can take screenshots of their interactions with the Chatbot and send to the Forum for Feedback, Reviews and Development Purposes.
